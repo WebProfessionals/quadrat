944cbfaec510570fcfd33f1547de1cd1a1a8bd38
veith <veithz@gmail.com>
2018-02-09 18:18:42 +0100

    editorconfig

--- geld.js
+++ geld.js
@@ -1,53 +1,53 @@
 class Geld {
 
-  constructor(initialWert, options) {
-    options = options || {};
-    this.waehrung = options.waehrung || 'Franken';
-    this.kommastelle = options.kommastelle || 2;
-    this.waehrungskuerzel = options.waehrungskuerzel || 'CHF';
-    this.value = initialWert || 0;
-  }
+    constructor(initialWert, options) {
+        options = options || {};
+        this.waehrung = options.waehrung || 'Franken';
+        this.kommastelle = options.kommastelle || 2;
+        this.waehrungskuerzel = options.waehrungskuerzel || 'CHF';
+        this.value = initialWert || 0;
+    }
 
-  valueOf() {
-    return this.value;
-  }
+    valueOf() {
+        return this.value;
+    }
 
-  toString() {
-    return this.waehrungskuerzel + ' ' + this.runden();
-  }
+    toString() {
+        return this.waehrungskuerzel + ' ' + this.runden();
+    }
 
-  /**
-   * Zieht vom Geldbetrag den Wert von wert ab
-   * @param wert Number oder Geld
-   * @returns {geld}
-   */
-  subtrahieren(wert) {
-    this.value -= wert;
-    return this;
-  }
+    /**
+     * Zieht vom Geldbetrag den Wert von wert ab
+     * @param wert Number oder Geld
+     * @returns {geld}
+     */
+    subtrahieren(wert) {
+        this.value -= wert;
+        return this;
+    }
 
-  /**
-   * Addiert zum Geldbetrag den Wert von wert dazu
-   * @param wert
-   * @returns {geld}
-   */
-  addieren(wert) {
-    this.value += wert;
-    return this;
-  }
+    /**
+     * Addiert zum Geldbetrag den Wert von wert dazu
+     * @param wert
+     * @returns {geld}
+     */
+    addieren(wert) {
+        this.value += wert;
+        return this;
+    }
 
-  /**
-   * Runden auf den 5er
-   */
-  runden() {
-    return Math.round(this.value * 20) / 20;
-  }
+    /**
+     * Runden auf den 5er
+     */
+    runden() {
+        return Math.round(this.value * 20) / 20;
+    }
 }
 
 
 let spesen = new Geld();
 
-let lohn = new Geld(13.99, {waehrung:'MonopolyFranken'});
+let lohn = new Geld(13.99, {waehrung: 'MonopolyFranken'});
 
 
 console.log(spesen);
